6eaabc080b5d36491d770d2379c447b05aa68a15
Fahd <36855117+fqhd@users.noreply.github.com>
2022-08-02 17:13:28 +0100

    Add neural network library

---
+++ include/Agent/NeuralNetwork.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <array>
+
+class NeuralNetwork {
+public:
+    NeuralNetwork(const std::vector<int>& model);
+    
+};
---
+++ src/NeuralNetwork.cpp
@@ -0,0 +1,6 @@
+#include "Agent/NeuralNetwork.hpp"
+#include <iostream>
+
+NeuralNetwork::NeuralNetwork(const std::vector<int>& model){
+    std::cout << "Configuring neural network" << std::endl;
+}
