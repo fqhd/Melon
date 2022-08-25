715963b079e95557aad74601e41d6e3def1951fa
Fahd <36855117+fqhd@users.noreply.github.com>
2022-08-25 10:32:51 +0100

    Random::randomInt()

--- include/Melon/Random.hpp
+++ include/Melon/Random.hpp
@@ -8,6 +8,7 @@ public:
     static void seed(uint32_t seed);
     static float randomFloat(float min, float max);
     static double randomDouble(double min, double max);
+    static int randomInt(int min, int max);
 
 private:
 
--- src/Random.cpp
+++ src/Random.cpp
@@ -7,11 +7,16 @@ void Random::seed(uint32_t seed){
 }
 
 float Random::randomFloat(float min, float max){
-    std::uniform_real_distribution<float> number(min, max); // Optimize this
+    std::uniform_real_distribution<float> number(min, max);
     return number(generator);
 }
 
 double Random::randomDouble(double min, double max){
-    std::uniform_real_distribution<double> number(min, max); // Optimize this
+    std::uniform_real_distribution<double> number(min, max);
+    return number(generator);
+}
+
+int Random::randomInt(int min, int max){
+    std::uniform_int_distribution<int> number(min, max);
     return number(generator);
 }
